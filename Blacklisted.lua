a4158359600bfc3491d925209b393ed40c802718
BackedUpMyData <108489553+BackedUpMyData@users.noreply.github.com>
2022-07-02 21:20:09 +0200

    update

--- Blacklisted.lua
+++ Blacklisted.lua
@@ -1,3 +1,5 @@
+-- // BANNED
 if game:GetService('Players').LocalPlayer.UserId == 3264568483 then
-    game:GetService('Players').LocalPlayer:Kick('\n ESG BANNED : Alt > Testing.')
+    game:GetService('Players').LocalPlayer:Kick('\n E.S.G. BANNED : Alt > Testing.')
 end
+--
